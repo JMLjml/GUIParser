124c8f5682e7d1d21c02f686e1800392477bd41e
JMLjml <johnlasheski@gmail.com>
2013-11-03 16:30:11 -0500

    Removing old file structure. I renamed the package from gui.builder to guibuilder. This cuased the file structure to be rebuilt

--- main/gui/builder/Lexer.java
+++
@@ -1,9 +0,0 @@
-package gui.builder;
-
-public class Lexer {
-
-	public Lexer() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- main/gui/builder/Parser.java
+++
@@ -1,9 +0,0 @@
-package gui.builder;
-
-public class Parser {
-
-	public Parser() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
